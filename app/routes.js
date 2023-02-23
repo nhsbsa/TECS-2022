a469400aef6bbb34ba43ac1566593440a5de9818
cbroa <charlotte.broad1@nhs.net>
2023-02-23 19:17:27 +0000

    added some routing

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,43 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+///////////////////////
+// ///V3 Routing///////
+//////////////////////
+
+router.get(/v3-postcode-input/, function (req, res) {
+    if (req.query.postcode == 'NE1 3JA') {
+      res.redirect('enquiry-letter-details');
+    } else if (req.query.postcode == 'NE2 4XL') {
+      res.redirect('enquiry-letter-details-copy');;
+    }
+    else {
+      res.redirect('cannot-find-your-details');
+    }
+  });  
+
+  router.get(/benefit-exemption/, function (req, res) {
+    if (req.query.benefit == 'yes) {
+      res.redirect('exemption-certificate-number');
+    } 
+    else if (req.query.benefit == 'no') {
+      res.redirect('did-you-have-an-exemption');;
+    }
+    
+  });  
+
+
+  router.get(/cert-number/, function (req, res) {
+    if (req.query.benefit == 'yes) {
+      res.redirect('exemption-certificate-number');
+    } 
+    else if (req.query.benefit == 'no') {
+      res.redirect('did-you-have-an-exemption');;
+    }
+    
+  }); 
+
+
+
+
 module.exports = router;
