2290af0e612087a4585764c4ad18237d156fe9cf
cbroa <charlotte.broad1@nhs.net>
2023-02-24 10:36:14 +0000

    added postcodes to index and added in more routing

--- app/routes.js
+++ app/routes.js
@@ -15,10 +15,55 @@ router.get(/v3-postcode-input/, function (req, res) {
     } else if (req.query.postcode == 'NE2 4XL') {
       res.redirect('enquiry-letter-details-copy');;
     }
+    else if (req.query.postcode == 'WA4 1AB') {
+      res.redirect('cannot-find-your-details');;
+    }
+    else if (req.query.postcode == 'WAH 3BE') {
+      res.redirect('more-information-needed');;
+    }
     else {
       res.redirect('cannot-find-your-details');
     }
   });  
+
+  router.get(/benefit-exemption/, function (req, res) {
+    if (req.query.exemption == 'yes') {
+      res.redirect('exemption-certificate-number');
+    }
+     else if (req.query.exemption == 'no') {
+     res.redirect('did-you-have-an-exemption');;
+     }
+   });
+
+  router.get(/cert-number/, function (req, res) {
+    if (req.query.number == 'yes') {
+      res.redirect('enter-exemption-certificate-number');
+    }
+     else if (req.query.number == 'no') {
+     res.redirect('check-name');;
+     }
+   });
+
+   router.get(/nhs-exemption/, function (req, res) {
+    if (req.query.nhs == 'med') {
+      res.redirect('check-name');
+    }
+    else if (req.query.nhs== 'mat') {
+      res.redirect('check-name');
+    }
+    else if (req.query.nhs == 'ppc') {
+      res.redirect('check-name');
+    }
+     else if (req.query.nhs == 'hc2') {
+       res.redirect('check-name');
+     }
+     else if (req.query.nhs == 'tc') {
+       res.redirect('check-name');
+     }
+     else if (req.query.nhs == 'no') {
+       res.redirect('were-you-claiming-any-benefits');
+     }
+   });
 
  // router.get(/benefit-exemption/, function (req, res) {
    // if (req.query.benefit == 'yes) {
@@ -32,6 +77,36 @@ router.get(/v3-postcode-input/, function (req, res) {
 
 
   
+   
+   router.get(/dwpexemptions-handler/, function (req, res) {
+    if (req.query.benefitsdwp == 'yes') {
+      res.redirect('dwp-check-name-uc');
+    }
+    else if (req.query.benefitsdwp == 'is') {
+      res.redirect('dwp-check-name-uc');
+    }
+    else if (req.query.benefitsdwp == 'jsa') {
+      res.redirect('dwp-check-name-uc');
+    }
+     else if (req.query.benefitsdwp == 'uc') {
+       res.redirect('dwp-check-name-uc');
+     }
+     else if (req.query.benefitsdwp == 'pc') {
+       res.redirect('dwp-check-name-uc');
+     }
+    
+     else if (req.query.benefitsdwp == 'no') {
+       res.redirect('dwp-we-need-proof-pecs');
+     }
+   });
+   router.get(/updatename/, function (req, res) {
+    if (req.query.details == 'yes') {
+      res.redirect('dwp-check-dob');
+    }
+     else if (req.query.details == 'no') {
+     res.redirect('dwp-update-name');;
+     }
+   });
 
 
 
