dc3c1f6522730f6fee9a3d5ec6e8e311848ae456
GeorgeRowley <george.rowley@nhsbsa.nhs.uk>
2025-02-14 12:19:02 +0000

    New UR journey error messaging added - WIP

--- app/views/user-research-feb-2025/_routes.js
+++ app/views/user-research-feb-2025/_routes.js
@@ -1,14 +1,15 @@
 const express = require('express')  
 const router = express.Router()
-
-// PLEASE NOTE: SOME ROUTES BELOW HAVE TEMPORARY VERY BASIC ERROR ROUTING, WILL NEED ADDING IN PROPER FUNCTIONAL DYNAMIC ERROR ROUTING IF NECESSARY
-
+const path = require('path');
 
 // Enter your reference number
 router.post('/enter-reference-number', function(request, response) {
-    var reference = request.session.data['reference-number']
-    if (reference === "georgerowley"){
-      response.redirect("error")
+    let reference = request.session.data['reference-number']
+    
+    if (!reference || reference.trim() === '') {
+      return response.render(path.join(__dirname, 'enter-reference-number'), {
+        formError: ' '
+      });
     } else {
       response.redirect("enter-reference-number-again")
     }
@@ -16,9 +17,12 @@ router.post('/enter-reference-number', function(request, response) {
 
 // Enter your reference number again
 router.post('/enter-reference-number-again', function(request, response) {
-  var referenceConfirm = request.session.data['reference-number-confirm']
-  if (referenceConfirm === "georgerowley"){
-    response.redirect("error")
+  let referenceConfirm = request.session.data['reference-number-confirm']
+  
+  if (!referenceConfirm || referenceConfirm.trim() === '') {
+    return response.render(path.join(__dirname, 'enter-reference-number-again'), {
+      formError: ' '
+    });
   } else {
     response.redirect("date-of-birth")
   }
@@ -26,10 +30,16 @@ router.post('/enter-reference-number-again', function(request, response) {
 
 // Date of birth
 router.post('/date-of-birth', function(request, response) {
-  var day = request.session.data['day']
-  var month = request.session.data['month']
-  if (day > 31 || month > 12){
-    response.redirect("error")
+  const currentYear = new Date().getFullYear();
+  let day = request.session.data['day']
+  let month = request.session.data['month']
+  let year = request.session.data['year']
+  
+  // Could do more advanced error checking or messaging but below is basic for now...
+  if (day > 31 || !day || month > 12 || !month || year > currentYear || !year ){
+    return response.render(path.join(__dirname, 'date-of-birth'), {
+      formError: ' '
+    });
   } else {
     response.redirect("enter-postcode")
   }
@@ -37,15 +47,35 @@ router.post('/date-of-birth', function(request, response) {
 
 // Enter your postcode
 router.post('/enter-postcode', function(request, response) {
-  var postcode = request.session.data['postcode']
+  let postcode = request.session.data['postcode']
 
   if (postcode === "NE2 4XL" || postcode === "NE24XL" || postcode === "NE1 3JA" || postcode === "NE13JA" ) {
     response.redirect("penalty-charge-notice-details")
+  } 
+  else if (!postcode || postcode.trim() === '') {
+    return response.render(path.join(__dirname, 'enter-postcode'), {
+      formError: ' '
+    });
   } else {
     response.redirect("cannot-find-your-details")
   }
 })
 
 // What you need to do next
+router.post('/what-you-need-to-do-next', function(request, response) {
+  let confirm = request.session.data['confirm']
+
+  if (confirm === "entitled") {
+    response.redirect("entitled")
+  } else if (confirm === "pay") {
+    response.redirect("pay")
+  } else if (confirm === "check") {
+    response.redirect("check")
+  } else {
+    return response.render(path.join(__dirname, 'what-you-need-to-do-next'), {
+      formError: ' '
+    });
+  }
+})
 
 module.exports = router
